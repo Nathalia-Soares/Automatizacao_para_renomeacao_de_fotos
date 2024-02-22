78932be5390ec8ac126b1c8554e220e9523a5a7b
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-02-22 03:26:33 -0300

    Atualização de cliques para a nova GUI do Win11

--- Automatização de fotos.py
+++ Automatização de fotos.py
@@ -1,32 +1,31 @@
 # Automatização de fotos
 
 import pyautogui
-import pyperclip
 import time
 
 pyautogui.PAUSE = 2
 
-caminhoOrigem = (input("Digite a pasta de origem "))
-quantidadeFotos = int(input("Quantas fotos existem no total? "))
+caminhoOrigem = (input("Digite a pasta de origem"))
+quantidadeFotos = int(input("Quantas fotos existem no total?"))
 
 # Acessando a pasta de origem
 
 pyautogui.click(x=988, y=1054)
-pyautogui.click(x=255, y=118)
+pyautogui.click(x=326, y=60)
 pyautogui.write(caminhoOrigem)
 pyautogui.press("enter")
 
-time.sleep(3);
+time.sleep(3)
 
 # Copiando as imagens da pasta
 
 pyautogui.click(x=318, y=212)
-pyautogui.hotkey("ctrl","a")
-pyautogui.hotkey("ctrl","c")
+pyautogui.hotkey("ctrl", "a")
+pyautogui.hotkey("ctrl", "c")
 
 # Acessando a pasta de destino
 
-pyautogui.click(x=1463, y=114)
+pyautogui.click(x=1338, y=60)
 pyautogui.write("F:\RC Morph\RC MORPH")
 pyautogui.press("enter")
 
@@ -34,7 +33,7 @@ pyautogui.press("enter")
 
 pyautogui.click(x=1900, y=989)
 pyautogui.click(x=318, y=212)
-pyautogui.hotkey("ctrl","a")
+pyautogui.hotkey("ctrl", "a")
 pyautogui.press("del")
 
 time.sleep(15)
@@ -42,23 +41,25 @@ time.sleep(15)
 # Colando as fotos na pasta de destino
 
 pyautogui.click(x=1900, y=989)
-pyautogui.hotkey("ctrl","v")
+pyautogui.hotkey("ctrl", "v")
+time.sleep(15)
 
 # Selecionando a primeira foto
 
 pyautogui.click(x=348, y=172)
 
-# Repetição para renomear as fotos
+# Repetição para renomear as fotos 
 
 for nomes in range(1 , quantidadeFotos + 1):
+    pyautogui.press("f2")
+    if nomes < 10:
+      pyautogui.write("0" + str(nomes))
+    else:
+      pyautogui.write(str(nomes))
+    time.sleep(1)
+    pyautogui.press("enter")
+    time.sleep(2)
+    pyautogui.press("right")
 
-   pyautogui.hotkey("shift", "f10")
-   pyautogui.press("up")
-   pyautogui.press("up")
-   pyautogui.press("enter")
-   if nomes < 10:
-      pyautogui.write("0"+str(nomes));
-   else:
-      pyautogui.write(str(nomes));      
-   pyautogui.press("enter")
-   pyautogui.press("right")
+
+
